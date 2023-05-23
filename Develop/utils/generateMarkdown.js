ce86e361e4a81a186b13b178820191903aadb6db
mgonzalezjr7 <miguel.gonzalezjr7@gmail.com>
2023-05-23 11:17:46 -0700

    Filled out the function for generateMarkdown.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -13,8 +13,37 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
-`;
+ 
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+ 
+  ## Description
+  ${data.description}
+ 
+  ## Installation
+  ${data.installation}
+ 
+  ## Usage
+  ${data.usage}
+ 
+  ## License
+  ${data.license}
+ 
+  ## Contributing
+  ${data.contributing}
+  ## Tests
+  ${data.tests}
+  ## Questions
+  If you have any questions, please contact me at:
+  - Email: ${data.email}
+  - Github: https://github.com/${data.github}
+  `;
 }
 
 module.exports = generateMarkdown;
